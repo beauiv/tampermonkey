1c1b2a5798789afb9bd462a555b5ff313d930596
beauiv <arthur.beau4@gmail.com>
2016-09-24 21:48:05 -0400

    First iteration
    Grabs gold automatically while playing.

---
+++ Auto-Grab Gold - Play Initium.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Auto-Grab Gold - Play Initium
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Grabs gold automatically when found.
+// @author       @beauiv_ (twitter)
+// @match        *http://www.playinitium.com/*
+// @match        *https://www.playinitium.com/*
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Check for gold
+    $(window).load(function() {
+        //ajax is fun, not sure of how to scan for gold without doing it every tenth of a second.
+        setInterval(function(){            
+        try{
+            var goldLink = $('a[onclick*="collectDogecoin"]');
+            if ( $(goldLink).attr("onclick") !== undefined)
+            {
+                //get gold
+                if( !$(goldLink).text().includes("Gold retrieved."))
+                {
+                $(goldLink).click();
+                $(goldLink).text("Gold retrieved.");
+                }
+            }
+            //else no gold is visible, don't try to get it
+            else
+            {
+                 //do nothing
+            }
+        }
+        
+
+        catch(e)
+        {
+            alert("Error collecting gold! Send to @beauiv_ : " + e.message);
+        }
+            },100);
+    }
+    
+    );
+})();
